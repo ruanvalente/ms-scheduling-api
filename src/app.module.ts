b35e6050cb9632b707d3e37c7d062d3539a54656
ruanvalente <ruanopensuselinux@gmail.com>
2025-10-04 13:04:57 -0300

    feat: add Professional and Scheduling modules to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './infra/database/database.module';
+import { SeedModule } from './infra/database/seed/seed.module';
+import { ProfessionalModule } from './modules/professionals/professional.module';
+import { SchedulingModule } from './modules/schedulings/schedulings.module';
 import { UserModule } from './modules/users/user.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [
+    DatabaseModule,
+    UserModule,
+    ProfessionalModule,
+    SchedulingModule,
+    ...(process.env.NODE_ENV === 'development' ? [SeedModule] : []),
+  ],
 })
 export class AppModule {}
